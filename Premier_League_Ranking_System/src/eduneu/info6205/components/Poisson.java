63f725c8f927dbd911e6866f009c890baeaa35ca
KarthikB96 <balasubramanya.k@husky.neu.edu>
2020-04-13 17:17:09 -0400

    Basic Poisson class

--- Premier_League_Ranking_System/src/eduneu/info6205/components/Poisson.java
+++ Premier_League_Ranking_System/src/eduneu/info6205/components/Poisson.java
@@ -2,5 +2,10 @@ package eduneu.info6205.components;
 import org.apache.commons.math3.distribution.PoissonDistribution;
 
 public class Poisson {
-
+	
+	
+	public static void demo() {
+	PoissonDistribution distribution = new PoissonDistribution(0.824);
+	System.out.println(distribution.probability(0));
+	}
 }
